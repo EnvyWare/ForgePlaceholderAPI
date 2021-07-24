d00715d1f4542e2bd512d30319aaad79ab8a358b
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-07-24 11:39:14 +0100

    Feat: implemented init for Forge

--- forge/src/main/java/com/envyful/papi/forge/ForgePlaceholderAPI.java
+++ forge/src/main/java/com/envyful/papi/forge/ForgePlaceholderAPI.java
@@ -1,5 +1,6 @@
 package com.envyful.papi.forge;
 
+import com.envyful.papi.api.local.LocalManagementLoader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
@@ -14,7 +15,7 @@ public class ForgePlaceholderAPI {
 
     @Mod.EventHandler
     public void onServerStarting(FMLPreInitializationEvent event) {
-
+        LocalManagementLoader.init("mods/ForgePlaceholderAPI/");
     }
 
     @Mod.EventHandler
