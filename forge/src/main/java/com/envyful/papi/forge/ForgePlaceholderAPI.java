dbaca5649da51204373ec0309bcfce0da244c084
Daniel Norris <dnorris443@gmail.com>
2021-11-22 16:55:55 +0000

    chore(forge): add updateJSON

--- forge/src/main/java/com/envyful/papi/forge/ForgePlaceholderAPI.java
+++ forge/src/main/java/com/envyful/papi/forge/ForgePlaceholderAPI.java
@@ -1,45 +1,26 @@
 package com.envyful.papi.forge;
 
 import com.envyful.api.forge.command.ForgeCommandFactory;
-import com.envyful.api.forge.concurrency.ForgeUpdateBuilder;
 import com.envyful.papi.api.local.LocalManagementLoader;
 import com.envyful.papi.forge.commands.PlaceholderCommand;
-import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
-import org.bstats.forge.Metrics;
-
-import java.nio.file.Paths;
 
 @Mod(
         modid = "forgeplaceholderapi",
         name = "Forge PlaceholderAPI",
         version = ForgePlaceholderAPI.VERSION,
-        acceptableRemoteVersions = "*"
+        acceptableRemoteVersions = "*",
+        updateJSON = "https://ogn.pixelmonmod.com/update/sm-fp/update.json"
 )
 public class ForgePlaceholderAPI {
 
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "1.0.0";
 
     @Mod.EventHandler
     public void onServerStarting(FMLPreInitializationEvent event) {
         LocalManagementLoader.init("mods/ForgePlaceholderAPI/");
-
-        Metrics metrics = new Metrics(
-                Loader.instance().activeModContainer(),
-                event.getModLog(),
-                Paths.get("config/"),
-                12198
-        );
-
-        ForgeUpdateBuilder.instance()
-                .name("ForgePlaceholderAPI")
-                .version(VERSION)
-                .requiredPermission("papi.update.notify")
-                .owner("Pixelmon-Development")
-                .repo("ForgePlaceholderAPI")
-                .start();
     }
 
     @Mod.EventHandler
