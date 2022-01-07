746f7587ca90ce136ceffaa1c0319fd724509c1c
Daniel Norris <dnorris443@gmail.com>
2022-01-07 21:06:13 +0000

    chore(gradle): bump version to 2.0.0

--- forge/src/main/java/com/envyful/papi/forge/ForgePlaceholderAPI.java
+++ forge/src/main/java/com/envyful/papi/forge/ForgePlaceholderAPI.java
@@ -16,7 +16,7 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 )
 public class ForgePlaceholderAPI {
 
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "2.0.0";
 
     @Mod.EventHandler
     public void onServerStarting(FMLPreInitializationEvent event) {
